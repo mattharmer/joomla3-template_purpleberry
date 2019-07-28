e4c010091f4d76aae90bd095db048af8743e84d5
Matt Harmer <harmer.matt@gmail.com>
2019-07-28 15:15:14 +1000

    index.php, google fonts.

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Craze Dance Academy</title>
+    <link rel="stylesheet" href="css/template.css">
+    <meta name="viewport" content="width=device-width"> <!--Make @media work in dev tools -->
+    <link href="https://fonts.googleapis.com/css?family=Pacifico|Raleway&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    
+  </body>
+</html>
