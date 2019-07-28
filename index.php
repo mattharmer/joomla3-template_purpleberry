b48805967529d4b19a85eb5042d2b8e4fae3e366
Matt Harmer <harmer.matt@gmail.com>
2019-07-28 15:58:16 +1000

    images added.

--- index.php
+++ index.php
@@ -8,6 +8,30 @@
     <link href="https://fonts.googleapis.com/css?family=Pacifico|Raleway&display=swap" rel="stylesheet">
   </head>
   <body>
-    
+    <div class="container">
+    <nav>
+      <title>Craze Dance Academy</title>
+      <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Enrolment</li>
+          <li>Classes</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      <header>
+        <img src="" alt="">
+        <h1>ENROL NOW FOR 2019 CLASSES</h1>
+        <a href="#">Learn More</a>
+      </header>
+      <footer>
+        <ul>
+          <li><a href="../facebook.svg">Facebook</a></li>
+          <li><a href="../instagram.svg">Instagram</a></li>
+          <li><a href="../twitter.svg">Twitter</a></li>
+          <li><a href="../youtube.svg">Youtube</a></li>
+        </ul>
+      </footer>
+    </div>
   </body>
 </html>
