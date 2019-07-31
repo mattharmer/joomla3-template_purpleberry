1e5ba599093a95c67fa86fbd4611b83b1dfad49c
Matt Harmer <harmer.matt@gmail.com>
2019-07-31 17:52:32 +1000

    edits

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <title>Craze Dance Academy</title>
     <link rel="stylesheet" href="css/template.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css?family=Pacifico|Raleway:400,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Dancing+Script:400,700|Raleway:400,900&display=swap" rel="stylesheet">
   </head>
   <body>
     <div class="container">
